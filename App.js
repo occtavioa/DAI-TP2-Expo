8a70348d704b0d18e26325b5a6ff3214710ee407
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-05 09:05:49 -0300

    imagenes

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.img1}
+        source={
+          require('./assets/imgs/central_perk_logo.jpeg')
+        }
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +23,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img1: {
+    height: 416,
+    width: 1080,
   },
 });
