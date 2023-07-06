19bd9091449a06973f6a1d19b06aaa347d008795
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-06 13:12:03 -0300

    nose

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image
         style={styles.img1}
-        source={
-          require('./assets/imgs/central_perk_logo.jpeg')
-        }
+        source={require("./assets/imgs/central_perk_logo.jpeg")}
       />
-      <Text>Open up App.js to start working on your app!</Text>
+      <div>
+        <Image
+          style={{height: 700, width: 1500}}
+          source={require("./assets/imgs/coffee1.jpeg")}
+        />
+        <Text
+          style={{position: 'relative'}}
+        >
+          Open up App.js to start working on your app!
+        </Text>
+      </div>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,12 +28,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   img1: {
     height: 416,
     width: 1080,
-  },
+  }
 });
