868ef696b0117bc4709bd2069e9fd545b9e64ef7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-07 11:17:01 -0300

    classes

--- App.js
+++ App.js
@@ -6,15 +6,17 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image
-        style={styles.img1}
-        source={require("./assets/imgs/central_perk_logo.jpeg")} />
-      <Text>Try some of our most popular flavors!</Text>
-      <View style={{height: 700, width: 1500, borderRadius: 30, overflow: 'hidden', flexDirection: 'row'}}>
+        style={styles.imgLogo}
+        source={require("./assets/imgs/central_perk_logo.jpeg")}
+      />
+      <Text style={styles.text}>Try some of our most popular flavors!</Text>
+      <View style={styles.containerImgCafe}>
         <Image
-          style={{height: 700, width: 1500, position: 'absolute'}}
-          source={require("./assets/imgs/coffee1.jpeg")} />
-        <View style={{position: 'absolute', backgroundColor: 'green', alignSelf: 'flex-end', width: '100%'}}>
-          <Text style={{alignSelf: 'center', color: 'white'}}>Mocha</Text>
+          style={styles.imgCafe}
+          source={require("./assets/imgs/coffee1.jpeg")}
+        />
+        <View style={styles.containerSubtitlo}>
+          <Text style={[styles.text, { color: "white" }]}>Mocha</Text>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -28,9 +30,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    height: "100%",
   },
-  img1: {
-    height: 416,
-    width: 1080,
-  }
+  imgLogo: {
+    aspectRatio: 1080 / 416,
+    width: "40%",
+  },
+  text: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  containerImgCafe: {
+    aspectRatio: 1 / 1,
+    borderRadius: 50,
+    overflow: "hidden",
+    flexDirection: "row",
+    width: "50%",
+  },
+  imgCafe: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  containerSubtitlo: {
+    position: "absolute",
+    backgroundColor: "green",
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    width: "100%",
+    height: "10%",
+  },
 });
